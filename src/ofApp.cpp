81176ef0743387348159244692c0e89a85cc33dc
Zacharias Vamvakousis <zackbam@gmail.com>
2018-05-22 16:47:05 +0200

    Added eyeharp.txt

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,6 +2,7 @@
 #include "ofApp.h"
 #include "stdio.h"
 #include "EyeHarpVARS.h"
+//global variables .......
 bool variables::alperMode, variables::record_chords, variables::triggerChords, variables::alperConfigureActive;
 int variables::firstNote, variables::notesPerScale, variables::harmonize,variables::textColor, variables::windowH, variables::windowW, variables::inReleaseFIXVEL;
 int variables::trNotesMidi[3];
@@ -29,7 +30,7 @@ void ofApp::setup(){
 	initParam = fopen("eyeharp.txt", "r");
 	char paramName[31];
 	float minVol;
-	int loadSong = 0, discNotesNumber = 15, stepSequencerNotesNumber = 6, bufferSize = 512, transpose = 0, FIXVEL = 70, loopBeLoopMIDI = 0;
+	int loadSong = 0, discNotesNumber = 13, stepSequencerNotesNumber = 6, bufferSize = 512, transpose = 0, FIXVEL = 70, loopBeLoopMIDI = 0;
 	bool replaySame=true,Breath=false,cc1 = 0, cc2 = 0, cc7 = 1, cc11 = 0,afterTouch=0, inRelease = false, semitoneActive = false, showScale = true, scalePreset = true,  chordsONOFF = false, mouseEyetribeInput = false, clickDwell = false, tomidi = false, fullscreen = false, monophonic = true, showGaze = true;
 	int temp;
 	variables::alperMode = false;
