205e701d70ba591da63fc45bb23ce19bbc75ca4d
zackbam <zackbam@gmail.com>
2020-07-13 19:34:05 +0300

    compiled with vs2019

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,28 @@ THE EyeHarp: BY ZACHARIAS VAMVAKOUSIS
 //========================================================================
 int main( ){
 	
-    ofAppGlutWindow window;
-	RECT desktop;
-	// Get a handle to the desktop window
-	const HWND hDesktop = GetDesktopWindow();
-	// Get the size of screen to the variable desktop
-	GetWindowRect(hDesktop, &desktop);
-//	ofSetupOpenGL(&window, 1024,575, OF_WINDOW);			// <-------- setup the GL context
-	ofSetupOpenGL(&window,desktop.right,desktop.bottom*0.92 , OF_WINDOW);			// <-------- setup the GL context
-	printf("Screen resolution: %d x %d\n", ofGetScreenWidth(), ofGetScreenHeight());
+//    ofAppGlutWindow window;
+//	RECT desktop;
+//	// Get a handle to the desktop window
+//	const HWND hDesktop = GetDesktopWindow();
+//	// Get the size of screen to the variable desktop
+//	GetWindowRect(hDesktop, &desktop);
+////	ofSetupOpenGL(&window, 1024,575, OF_WINDOW);			// <-------- setup the GL context
+//	ofSetupOpenGL(&window,desktop.right,desktop.bottom*0.92 , OF_WINDOW);			// <-------- setup the GL context
+//	printf("Screen resolution: %d x %d\n", ofGetScreenWidth(), ofGetScreenHeight());
+//	// this kicks off the running of my app
+//	// can be OF_WINDOW or OF_FULLSCREEN
+//	// pass in width and height too:
+//	ofRunApp( new ofApp());
+
+
+
+	ofSetupOpenGL(1366, 768, OF_WINDOW);            // <-------- setup the GL context
+
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new ofApp());
+	//globalVars::AsioOn = false;
 
+	ofRunApp(new ofApp());
 }
